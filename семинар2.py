3c4052a7ae016b9c183574c4865b8e2cb7577ba3
katesudnik <124512375+katesudnik@users.noreply.github.com>
2023-04-16 20:46:56 +0300

    Add files via upload

---
+++ семинар2.py
@@ -0,0 +1,21 @@
+# Задача 12
+# s, p = map(int, input().split())
+# x = (s-int((s**2-4*p)**0.5))//2
+# y = (s+int((s**2-4*p)**0.5))//2
+# print(x, y)
+
+# Задача 10:
+# n = int(input())
+# k = 0
+# for i in range(n):
+#     v = int(input())
+#     if v == 1:
+#         k += 1
+# print(k if k<n/2 else n-k)
+
+# Задача 14
+# n = int(input())
+# m = 1
+# while m < n:
+#     print (m,end=' ')
+#     m = m * 2
